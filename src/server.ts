ac16d6400ee49aca8fb179e5346b5a8928e4ea76
Felix Jenkel (FG-432) <12564929+felixjenkel@users.noreply.github.com>
2020-10-05 17:45:15 +0200

    upload

---
+++ src/server.ts
@@ -0,0 +1,79 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { Request, Response } from 'express';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8080;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  // IT SHOULD
+  //    1
+  //    1. validate the image_url query
+  //    2. call filterImageFromURL(image_url) to filter the image
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+  /**************************************************************************** */
+
+  //! END @TODO1
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    await res
+      .send("try GET /filteredimage?image_url={{}}")
+      .status(200)
+  } );
+
+  app.get( "/filteredimage", async ( req: Request, res: Response ) => {
+      console.log(req);
+      let path: string;
+      let image_url: string = decodeURIComponent(req.query.image_url);
+      console.log(image_url);
+      const prom = filterImageFromURL(image_url);
+      prom.then((img_path) => {
+        console.log("resolved wird gecalled");
+        console.log(img_path);
+        res
+          .status(200)
+          .sendFile(img_path);
+
+        res.on('finish', function(){
+            deleteLocalFiles(img_path)
+              .then(() => {
+                  console.log("Loeschen war erfolgreich")})
+              .catch((err) => {
+                  console.log(err);
+                  console.log("Löschen war nicht erfolgreich")});
+        });
+      });
+      prom.catch((err) => {
+        console.log(err);
+        console.log("Catch wird gecalled");
+      });
+
+
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
